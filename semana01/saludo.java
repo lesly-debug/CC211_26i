bcd92bfbc81d902d825fc2c6c926f77f84e285e7
lesly-debug <bltzrlesly@gmail.com>
2026-03-27 15:46:17 +0000

    Ejercicios en java

---
+++ semana01/saludo.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class saludo{
+public static void main(String[] args){
+	Scanner leer=new Scanner(System.in);
+	System.out.print("Ingrese la primera cadena: ");
+	String cad1=leer.nextLine();
+	System.out.println(cad1);
+	System.out.print("Ingrese la segunda cadena: ");
+	String cad2=leer.nextLine();
+	System.out.println(cad2);
+	}
+}
