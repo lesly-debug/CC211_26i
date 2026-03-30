67d82eada3c057d4b76b90fed35bc493415a5f1a
lesly-debug <bltzrlesly@gmail.com>
2026-03-30 19:18:55 +0000

    Conceptos de POO

--- semana01/Saludo.java
+++ semana01/Saludo.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class saludo{
+public class Saludo{
 public static void main(String[] args){
 	Scanner leer=new Scanner(System.in);
 	System.out.print("Ingrese la primera cadena: ");
